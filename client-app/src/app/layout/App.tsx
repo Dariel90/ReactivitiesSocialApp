9c5f7f5bca3925df4e60b972c3b38357905b45db
Dariel <dariel901231@gmail.com>
2020-01-31 11:50:03 -0500

    Uploading resources. Creating NavBar and ActivityDashboard components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,41 +1,33 @@
-import { Component } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import React from 'react';
 import axios from "axios";
-import { Header, Icon, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
 
-interface IState {
-  activities: IActivity[];
-}
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: [],
-  }
 
-  componentDidMount(){
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response) => {
-      this.setState({
-        activities: response.data
-      });
+      setActivities(
+        response.data
+      );
     })
-  }
+  }, []);
 
-  render(){
-    return (
-      <div>
-          <Header as='h2'>
-            <Icon name='users' />
-            <Header.Content>Reactivities</Header.Content>
-          </Header>
-          <List>
-            {this.state.activities.map((activity) => (
-              <List.Item key={activity.id}>{activity.title}</List.Item>
-            ))}
-          </List>
-      </div>
-    );
-  }
+  return (
+    <Fragment>
+        <NavBar />
+        <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard activities={activities}/>
+        </Container>
+    </Fragment>
+  );
 }
 
 export default App;
